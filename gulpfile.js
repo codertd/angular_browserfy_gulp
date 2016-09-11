e2a385c43111996ddd2c7430a280542c35ddb9b2
Tim Doyle <coder.td@gmail.com>
2016-09-11 15:23:05 -0700

    Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp')
 var connect = require('gulp-connect')
 var sass = require('gulp-sass');
 
-
+// requires browserify and vinyl-source-stream
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
 
 
 gulp.task('styles', function() {
@@ -10,6 +12,18 @@ gulp.task('styles', function() {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/www/css/'));
 });
+
+
+gulp.task('browserify', function() {
+	// Grabs the app.js file
+    return browserify('./app/scripts/**/*.js')
+    	// bundles it and creates a file called main.js
+        .bundle()
+        .pipe(source('main.js'))
+        // saves it the dist/www/js/ directory
+        .pipe(gulp.dest('./dist/www/js/'));
+})
+
 
 gulp.task('connect', function () {
 	connect.server({
@@ -22,4 +36,6 @@ gulp.task('connect', function () {
 //Watch task
 gulp.task('default',function() {
     gulp.watch('app/styles/**/*.scss',['styles']);
+
+    gulp.watch('app/scripts/**/*.js',['browserify']);
 });
