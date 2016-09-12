6873dbf5e6379653b08efc2567fab99e92c3e0f9
Tim Doyle <coder.td@gmail.com>
2016-09-11 17:07:59 -0700

    Browserify now doing an uglify.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 require('jquery')
 require('angular')
+
 var MainController = require('./controllers/MainController')
+
 
 var app = angular.module('app', [])
 app.controller('MainController', ['$scope', MainController])
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp')
 var connect = require('gulp-connect')
 var sass = require('gulp-sass');
 
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var jshint = require('gulp-jshint')
+var uglify = require('gulp-uglify')
+var pump = require('pump')
+
+
 // requires browserify and vinyl-source-stream
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
@@ -20,12 +28,24 @@ gulp.task('copy-html', function() {
 });
 
 
+gulp.task('compress-js', function (cb) {
+  pump([
+        gulp.src('/dist/www/js/main.js'),
+        uglify(),
+        gulp.dest('dist/www/js/main.min.js')
+    ],
+    cb
+  );
+});
+
 gulp.task('browserify', function() {
 	// Grabs the app.js file
     return browserify('./app/scripts/main.js')
     	// bundles it and creates a file called main.js
         .bundle()
         .pipe(source('main.js'))
+        .pipe(buffer()) // <----- convert from streaming to buffered vinyl file object
+        .pipe(uglify()) // now gulp-uglify works
         // saves it the dist/www/js/ directory
         .pipe(gulp.dest('./dist/www/js/'));
 })
